727e8a484836cc31e23153962232e35fc3d83acb
zhangxueliang <zhangxueliang@dameng.com>
2023-04-13 22:16:02 +0800

    first commit

---
+++ src/test/java/com/zxl/magicapidemo/MagicApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zxl.magicapidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicApiDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
